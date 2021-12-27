84455b49553107db448a115c51cd63856fd7e519
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-27 13:15:30 +0530

    for-loop-Conc-cS

---
+++ Varsha.java
@@ -0,0 +1,7 @@
+class Varsha{
+public static void main (String [] args){
+String[]Months={"January","Febuary","March","April","May","June","July","August","September","October","November","December"};
+System.out.println(Months[0]+" "+Months[1]+" "+Months[2]+" "+Months[3]+" "+Months[4]+" "+Months[5]+" "+Months[6]+" "+Months[7]+" "+Months[8]+" "+Months[9]+" "+Months[10]+" "+Months[11]);
+}
+}
+}
---
+++ Weekdina.java
@@ -0,0 +1,6 @@
+class Weekdina{
+public static void main (String [] args){ 
+String []week={"sunday", "monday","tuesday","wednesday","thursday","friday","saturday"};
+System.out.println(week[0]+" "+week[1]+" "+week[2]+" "+week[3]+" "+week[4]+" "+week[5]+" "+week[6]);
+}
+}
